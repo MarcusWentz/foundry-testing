423789098ced2ee39a53c4abecaa01f65a4960f9
marcuswentz <markzcollege404@gmail.com>
2022-12-28 22:57:19 -0500

    Foundry tested set and owner functions

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,15 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.7;
 
 contract SimpleStorage {
-    uint public storedData; //Do not set 0 manually it wastes gas!
 
-    event setEvent();
+    uint  public storedData;  //Do not set 0 manually it wastes gas!
+    uint  public ownerUnixTimeContract; 
+    address public immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    event setOpenDataEvent(address indexed user, uint newValue); //Topics and other event arguments used for Foundry testing. Event arguments like this use gas in production so be careful.
+    event setOwnerDataEvent(uint newOwnerUnixTime);
+    event donateToOwnerEvent();
+
 
     function set(uint x) public {
-        require(storedData != x, "CANNOT_BE_SAME_VALUE");
+        require(storedData != x, "CANNOT_BE_SAME_VALUE"); //WRITE AS CUSTOM ERROR!
         storedData = x;
-        emit setEvent();
+        emit setOpenDataEvent(msg.sender, x); //Topic 1 (user) and other argument not indexed (newValue) for Foundry.
     }
+
+    function setOwnerData() public {
+        require(msg.sender == owner, "YOU_ARE_NOT_THE_OWNER!"); //WRITE AS CUSTOM ERROR!
+        ownerUnixTimeContract = block.timestamp;
+        emit setOwnerDataEvent(block.timestamp);
+    }
+
+    // function donateToOwner() public payable {
+    //     require(msg.value > 0, "MSG.VALUE_IS_ZERO!"); //WRITE AS CUSTOM ERROR!
+    //     payable(owner).transfer(address(this).balance);
+    //     emit donateToOwnerEvent();
+    // }
 
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+pragma solidity 0.8.7;
 
 import "forge-std/Test.sol";
 
@@ -12,28 +12,49 @@ contract TestContract is Test {
         simpleStorageInstance = new SimpleStorage();
     }
 
-    // function testBar() public {
-    //     assertEq(uint256(1), uint256(1), "ok");
-    // }
-
-    // function testFoo(uint256 x) public {
-    //     vm.assume(x < type(uint128).max);
-    //     assertEq(x + x, x * 2);
-    // }
-    
-    function testValidPath() public {
+    function testInitialStorage() public {
         assertEq(simpleStorageInstance.storedData(),0);
+        assertEq(simpleStorageInstance.ownerUnixTimeContract(),0);
+        assertEq(simpleStorageInstance.owner(),address(this));
+    }
+
+    function testSetValidPath() public {
+        assertEq(simpleStorageInstance.storedData(),0);
+        vm.expectEmit(true,false,false,true); // Events have bool flags for indexed topic parameters in order (3 topics possible) along with arguments that might not be indexed (last flag). You can also check which address sent the event.
+        emit setOpenDataEvent(address(this),1); 
         simpleStorageInstance.set(1);
-        //TEST EVENTS, TOPIC INDEX IN ORDER, OTHERWISE IF OTHER ARGUMENTS NOT INDEXED LEAVE THEM AS BLANK.
         assertEq(simpleStorageInstance.storedData(),1);
     }
 
-    function testRevertPath() public {
+    function testSetRevertPath() public {
         assertEq(simpleStorageInstance.storedData(),0);
         vm.expectRevert();    
         simpleStorageInstance.set(0);
         assertEq(simpleStorageInstance.storedData(),0);
     }
 
+    function testSetOwnerDataValidPath() public {
+        assertEq(address(this),simpleStorageInstance.owner());
+        assertEq(simpleStorageInstance.ownerUnixTimeContract(),0);
+        vm.expectEmit(false,false,false,true); // Events have bool flags for indexed topic parameters in order (3 topics possible) along with arguments that might not be indexed (last flag). You can also check which address sent the event.
+        emit setOwnerDataEvent(10);
+        vm.warp(10);   //Increase block.timestamp by 10 seconds.
+        simpleStorageInstance.setOwnerData();
+        // vm.stopPrank(); //Stop prank as contract address. We are already the owner address.
+        assertEq(simpleStorageInstance.ownerUnixTimeContract(),10);
+    }
+
+    function testSetOwnerDataRevertPath() public {
+        vm.startPrank(address(0)); //Change the address to not be the owner. The owner is address(this) in this context.
+        assertEq(simpleStorageInstance.ownerUnixTimeContract(),0);
+        vm.expectRevert();    
+        simpleStorageInstance.setOwnerData();
+        // vm.stopPrank(); //Stop prank as contract address. We revert before hitting this line so we don't need to stop the prank.
+    }
+
+    //Define events here for from other contracts since Foundry has trouble importing events from other contracts still.
+    event setOpenDataEvent(address indexed user, uint newValue);
+    event setOwnerDataEvent(uint newOwnerUnixTime);
+    
 }
 
